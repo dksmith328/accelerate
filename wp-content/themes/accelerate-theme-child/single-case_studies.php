ad42800baf788bd15e84a43b5c3bf1d119ddefdc
dksmith328 <dksmith328@gmail.com>
2018-12-27 09:50:08 -0700

    first commit

---
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="site-content sidebar">
+		<div class="main-content" role="main">
+
+			<?php while ( have_posts() ) : the_post();
+				$services = get_field('services');
+				$client = get_field('client');
+				$link = get_field('site_link');
+				$image_1 = get_field('image_1');
+				$image_2 = get_field('image_2');
+				$image_3 = get_field('image_3'); ?>
+
+
+	<article class="case_study">
+			<aside class="case-study-sidebar">
+				<h2><?php the_title(); ?></h2>
+				<h5><?php echo $services; ?></h5>
+				<h6>Client: <?php echo $client; ?></h6>
+
+			<?php the_content (); ?>
+
+			<p><a target="_blank" href="<?php echo $link; ?>">Site Link</a></p>
+			</aside>
+
+			<div class="case-study-images">
+				<img class="alignright" src="<?php echo $image_1; ?>" />
+				<img class="alignright" src="<?php echo $image_2; ?>" />
+				<img class="alignright" src="<?php echo $image_3; ?>" />
+			</div>
+
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+
+
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
